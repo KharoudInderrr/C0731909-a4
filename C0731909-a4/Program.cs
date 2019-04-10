f2ab2c26ef490d5f647ce8f0cde317a4ba4b1bf6
visualstudio <visualstudio@122-PC18>
2019-04-10 08:42:01 -0400

    committed

--- C0731909-a4/Program.cs
+++ C0731909-a4/Program.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+
+
 
 namespace C0731909_a4
 {
     class Program
     {
+        ArrayList Beowulf;
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
+
+        }
+
+        public void Run()
+        {
+            this.ReadTextFiles();
+        }
+        public void ReadTextFiles()
+            {
+
         }
     }
 }
